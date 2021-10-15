369e025425638c8434651c928c1bfe1024899f3c
Necdet UYGUR <necdet.uygur@gmail.com>
2021-10-15 17:05:50 +0300

    Dosyalar parçalara ayrıldı.

---
+++ app.js
@@ -0,0 +1,46 @@
+const ZAMAN = "2021-12-18 00:00:00";
+
+window.addEventListener("load", function () {
+  SozTick();
+  ZamanFarkiTick();
+});
+
+function ZamanFarki(zaman) {
+  zaman = zaman.replace(/-/gi, "/");
+  zaman = new Date(zaman) * 1;
+  simdi = new Date() * 1;
+  bir_saniye = 1000;
+  bir_dakika = 60 * bir_saniye;
+  bir_saat = 60 * bir_dakika;
+  bir_gun = 24 * bir_saat;
+  bir_hafta = 7 * bir_gun;
+  fark = zaman > simdi ? zaman - simdi : simdi - zaman;
+  gun_farki = Math.floor(fark / bir_gun);
+  saat_farki = Math.floor((fark % bir_gun) / bir_saat);
+  dakika_farki = Math.floor((fark % bir_saat) / bir_dakika);
+  saniye_farki = Math.floor((fark % bir_dakika) / bir_saniye);
+  return {
+    gun: gun_farki,
+    saat: ("0" + saat_farki).slice(-2, 3),
+    dakika: ("0" + dakika_farki).slice(-2, 3),
+    saniye: ("0" + saniye_farki).slice(-2, 3),
+    fark: fark,
+  };
+}
+
+function ZamanFarkiTick() {
+  var zf = ZamanFarki(ZAMAN);
+  $("#zaman-farki").html(
+    zf.gun + " gün, " + zf.saat + ":" + zf.dakika + ":" + zf.saniye
+  );
+  setTimeout(ZamanFarkiTick, 1000);
+}
+
+function SozTick() {
+  $("#soz")
+    .fadeOut(5e3, function () {
+      $(this).html(GetSoz());
+    })
+    .fadeIn(2e3);
+  setTimeout(SozTick, 15e3);
+}
---
+++ soz.js
@@ -0,0 +1,55 @@
+const SOZ_LIST = `Sigara iradeyle değil mantıkla bırakılır.
+Rejim yapmak yiyeceklere verilen değeri azaltmaz.
+İlk başta denemek istemiş olabiliriz ama kimse hayat boyu sigara içmeyi seçmez.
+Tüm sigara içenler çocuklarının sigara başlamamasını ümit eder, kendileri de hiç başlamamış olmayı isterlerdi.
+Sigarayı sadece istediğinizde içmezsiniz, içmek zorundasınızdır ve içmeme düşüncesi bile sizi endişelendirir.
+Sigaraya başlamayı ne zaman seçtiniz? Sigaraya devam etmeyi de seçtiniz mi?
+Sigara içenlerin büyük kısmı 'Aslında içmiyor olmayı tercih ederdim.' derler. Yani sigara içmek kendi seçimleri değildir. Sigara içmek bir bağımlılıktır. Seçme özgürlüğünüzü elinizden alır, tiryakiyi köleleştirir. Seçme şansları olsa sigara içenler, içmemeyi tercih ederler.
+Hangi aşamada sigara tuzağına düştünüz? Kaçamayacağınızı anladığınız anda mı? İlk sigaranıza geri dönün, hoşunuza gitmediğine göre şöyle düşündünüz; bağımlı olmayacağım. Ama olmuştunuz bile, çünkü nikotin tek seferde bağımlılık yapan bir güven tuzağıdır. Tuzağa çekiliriz ve kurtulmaya çalışana kadar bağımlılığımızın farkına bile varmayız.
+Zevkini aldığınız şey sigara içmeyen biriyken sahip olduğunuz mutluluk seviyesine geri dönme çabasıdır. Bu basit hile sigara içenleri bağımlı yapan şeydir. Gerçek şudur ki, sigara içen kişi asla sigaraya başlamadan önceki mutluluk seviyesine ulaşamaz.
+İçmemenizi değil içmenizi gerektiren nedenlerden sigara içiyorsunuz.
+Sigara içme nedenlerinizin tümü birer yanılsamadır.
+Sigara içenler gerekçe olarak çelişkili nedenler öne sürer. Mutluluktan ya da üzüntüden, stres altında olduklarından ya da rahat bir ortamda olduklarından, odaklanmak için ya da kafa dağıtmak için...
+Tadın sigara içmekle hiçbir ilgisi yoktur, sigarayı yemezsiniz. Istakozun tadını ne kadar çok severseniz sevin yanınızda taşıyacak duruma gelmezsiniz.
+Kokusunu seviyorsanız neden evinizi sigaranın hoş kokusuyla doldurmuyorsunuz? Aslında berbat kokusu sigara içenlerin bırakmak istemesinin nedenlerinin biridir. Hatta sigara içenlerin çoğu başkalarının sigara kokusundan hoşlanmaz.
+Gülün kokusunu sevseniz de bu sizde gül içme isteği uyandırmaz.
+Bunun el ya da ağız alışkanlığı olduğunu ya da dudak tiryakisi olduğunuzu düşünüyorsanız o zaman neden yakıyorsunuz ki? Sadece tutun ya da sadece dudaklarınıza götürün. Böylece sağlığınıza hiç zarar vermeden paranızı da kurtarabilirsiniz.
+Duman çekmeyi seviyorsanız, bahçenizde kuru otlar yakın ve dumanını içinize çekin. Buna boğulmak denir. Sigaranın en kötü yanı, içerken kendinizi boğarak öldürmek zorunda kalmanızdır. Peki, sigara dumanını özel kılan ne? Çünkü nikotin içerir ve bu madde bağımlılık yapar.
+Eroin bağımlıları iğne yapma ritüelini sevdiklerine inanırlar. Ama sevmezler, bunu sadece uyuşturucuyu vücuda vermenin bir yolu olduğu için ve bir önceki alımın azalan etkisini gidermek için yaparlar. Sigara içenlerde aynı bu durumdadır.
+Sigara içiyorsanız iki tip korku arasında kalırsınız. Bir tarafta 'Beni öldürüyor, çok para gidiyor, pis, iğrenç ve hayatımı kontrol ediyor.' korkusu var, diğer tarafta 'O küçük zevkim, desteğim olmadan hayatın tadını nasıl çıkarırım, stresle nasıl baş ederim.' korkusu.
+Sigarayı bırakırsanız korkunç bir travma geçireceğinizden, başarsanız bile içme isteğinizden asla tam olarak kurtulamayacağınızdan korkuyorsunuz. Sigara içenler bu iki tip korkunun da nedeninin sigara içmek olduğunu anlayamaz. Sigara içmeyenler bu korkuları zaten hiç yaşamazlar. Sigara içenlerin fark etmeleri gereken şey budur.
+Sigarayı bırakma konusundaki korkular, ömür boyu beyninizin yıkanmasının bir sonucudur.
+Tütündeki nikotin insanoğlunun bildiği en çok bağımlılık yapan uyuşturucudur. İyi olan şu ki nikotin çok bağımlılık yapsa da fiziksel kesilme hafif olur. O kadar hafiftir ki sigara içenlerin çoğu bunu uyuşturucu bağımlılığı olarak görmez. Aslında oluşan boşluk hissi açlık ve normal stresin verdiği hisse benzer. Hiçbir fiziksel acı yoktur. O yüzden beynimiz bu hisse normal hayat stresinin yol açtığı yanılgısına kapılır. Sigarayı bırakanların bu ayrımı kavraması gerekir.
+Sigara içenler bırakmayı denediklerinde yaşadıkları sıkıntıyı krize girmek sanır. Ama bu fiziksel değil zihinseldir. Bu zihinsel krizi daha son sigaranızı söndürmeden ortadan kaldırmak mümkündür.
+Sigara içenler krize sadece sigarayı bırakmak istediklerinde girdiklerini düşünür. Aslında sigara içtikleri sürece durum böyledir. Her sigara istiyorum ya da ihtiyacım var dediğinizde bir sigara krizine girmişsiniz demektir. Bu belli belirsiz boşluk, güvensizlik duygusunu sigara içenler şöyle bilir: 'Ellerimle bir şey yapmalıyım ya da sigara istiyorum ya da ihtiyacım var.' Bu aslında nikotin krizidir.
+Sigara içtiğinizde bile nikotin krizi tamamen kesilmez. O belli belirsiz boşluk, güvensizlik hissi asla tamamen ortadan kaybolmaz. Sigara içerken bile, sigaraya başlamadan önceki halinize göre çok daha gerginsinizdir. Bu yüzden bilinçli beyniniz daha az içmeniz gerektiğini söylese bile, doğal eğiliminiz hep daha fazla içmektir.
+Sigara içmek, çıkarınca rahatlamak için ayağı sıkan bir ayakkabıyı giymeye benzer.
+Yarın bir otobüste çarpabilir diyebilirsiniz, peki kasıtlı olarak önüne atlar mısınız?
+Egzoz dumanları sigara içmekten daha çok zarar veriyor, peki azınızı bir egzoz borusuna dayayıp bilerek dumanları çeker misiniz?
+Sigara alacak paranız var, sizin adınıza sevindim; ama 50.000 sterlini yakmak biraz aptalca değil mi?
+Arabamıza vücudumuz gibi davransaydık, bir yılda hurdaya dönerdi.
+Sigara içen biri içmeyene göre iyi gününde daha az mutlu, kötü gününde daha stresli olur.
+Sigara sizi rahatlatmaz, tam tersi olur. Sigara sizi telaşlı, sinirli ve gergin yapar. Sigara içmenizin nedeni zaten sigaranın kendisinin yarattığı krizi hafifletmektir. Aslında sigara içmeyen biri kadar rahat olmak için içersiniz ama içmeyen biri krize zaten girmez.
+Sigara endişeyi azaltmaz, endişeye neden olur. Örneğin sigaranız azaldığında nasıl telaşlandığınızı bir düşünün. Ama sigara içmeyenlerde bu olmaz.
+Sigaradan kaynaklanan uyuşukluk ve kayıtsızlık kronik can sıkıntınızın ana nedenidir.
+Bildiğiniz gibi sigara konsantrasyona yardımcı olur. Ve bu iş de yüksek konsantrasyon gerektirir. Sigara içmediğinizi fark ettim. Eğer sigaraya başlamayacaksanız sizi işe alamam.
+Bu tabii ki saçma bir durum. Gürültü, açlık ya da başka bir şey dikkatinizi dağıtıyorsa konsantre olabilmek için o şeyi ortadan kaldırırsınız. Her sigara istediğinizde aslında dikkatiniz dağılıyor demektir.
+Sigara içenlerin yaşadığı gerçek stres fiziksel kriz yüzünden değildir. Neyse ki bu kriz belli belirsiz hissedilir. Zaten sigarayı bırakmak bu yüzden kolaydır işte. Ama sigarayı arzulamak ve içmemek insana daha büyük bir stres yaşatır. Tıpkı kaşınan bir yeri kaşıyamamak gibi.
+Sigarayı bıraktığınızda bağımlılığınız kötüye gitmez.
+Sigara içenler bunun sadece bir alışkanlık olduğu yanılgısına kapılır. Ama sigara içmek alışkanlık değil uyuşturucu bağımlılığıdır.
+Tuzağı anlarsanız bağımlılıktan kurtulmak çok kolaydır.
+Sigaranın yararından çok zararı var demiyorum. Demek istediğim sigaranın hiçbir yararının olmadığıdır. Sigarayı bıraktığınızda hiçbir fedakarlıkta bulunmazsınız.
+Beyninizde süren çekişmede, taraflardan birini yok ettiğinizde bırakmak için iradeye ihtiyacınız olmaz.
+Sigara yerine konan her şey bırakmayı zorlaştırır. Çünkü bir fedakarlık yaptığınız yanılsamasına neden olurlar. Gribi atlattığınızda yerine bir şey koymak istemezsiniz.
+Sadece bir sigara diye bir şey yoktur. Yanıtlamanız gereken 'Sigara içsem mi?' sorusu değil, 'Tekrar sigara içen biri olmak istiyor muyum?' sorusudur.
+Sigara içmeyen biri olarak kalmak için gereken tek şey bir daha sigara içmemektir.`.split(
+  "\n"
+);
+
+function GetSoz() {
+  var soz = null;
+  while (!soz) {
+    soz = SOZ_LIST[(Math.random() * SOZ_LIST.length).toFixed(0) * 1];
+  }
+  return soz;
+}
